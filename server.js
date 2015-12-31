6600756f8d875ab80af14f4fff4a07837004f5ee
venkatesh pallanti <venkyoct6@gmail.com>
2015-12-31 13:27:10 +0530

    Initialize

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+var middleware = {
+	requireAuthentication: function(req,res,next){
+		console.log('private info');
+		next();
+	},
+	logger:function(req,res,next){
+     console.log('Requested to '+ new Date().toString() + ' ' +req.method + ' ' + req.originalUrl);
+     next();
+	}
+};
+app.use(middleware.logger);
+app.get('/about',middleware.requireAuthentication,function(req,res){
+res.send('About');
+});
+app.use(express.static(__dirname+'/public'));
+app.listen(PORT,function(){
+console.log('server is running on port'+PORT);
+});
